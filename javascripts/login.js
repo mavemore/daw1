ca24b6d7ea0975a0ddfa3298339b80725ed2126d
mavemore <veronica940413@hotmail.com>
2016-07-03 17:10:35 -0500

    login discriminatorio
    solo cliente y operario

--- javascripts/login.js
+++ javascripts/login.js
@@ -6,8 +6,11 @@ $(document).ready(function(){
 function init(){
 	$("#btnSubmit").click(function()
 	{
-		var tipoUsuario = $("select option").val();
-		if( tipoUsuario == "Cliente")
+		var tipoUsuario = $("select option:selected").text();
+		if( tipoUsuario == "Cliente"){
 			window.location.href = "usuarios.html";
+		}else if( tipoUsuario == "Operario"){
+			window.location.href = "operarios.html";
+		}
 	});
 }
