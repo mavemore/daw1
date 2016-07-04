d98e07698f51d860163e8e483e4845e31e9362d9
mavemore <veronica940413@hotmail.com>
2016-07-04 14:24:56 -0500

    reportes y busqueda muestras
    html de reportes y modifcar muestras en operarios

--- javascripts/operarios.js
+++ javascripts/operarios.js
@@ -2,10 +2,20 @@ $(document).ready(function(){
 	init();
 });
 
-
 function init(){
 	$("header button").click(function()
 	{
 		window.location.href = "index.html";
+	});
+	
+	$(function() {
+		$('#pickDesde').datetimepicker({
+		  pickTime: false
+		});
+	});
+	$(function() {
+		$('#pickHasta').datetimepicker({
+		  pickTime: false
+		});
 	});
 }
